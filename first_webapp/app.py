a7c55e679d175221b200e01794e336c2017dfb76
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 11:44:39 +0900

    streamlit

---
+++ first_webapp/app.py
@@ -0,0 +1,15 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """ 
+    # 제 첫 웹페이지입니다.
+    ## 부족하지만 많이 사랑해주십쇼
+    ### 1$ 는 1300원 입니다.
+    """
+)
+
+st.image(
+    "https://pixabay.com/illustrations/earth-space-sunlight-sun-rays-1756274/"
+)
