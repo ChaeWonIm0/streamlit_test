85c4283f5fc83257d21f845aeccd8fed29987943
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 12:32:27 +0900

    st.title

--- lucky/app.py
+++ lucky/app.py
@@ -2,60 +2,11 @@
 import streamlit as st
 import numpy as np
 
+# 추첨 대상인 13명의 이름을 넣을 수 있는 
+# 13명이 소속될 조 이름을 넣을 위치
 
-st.write(
-    """ 
-    # 세상에서 가장 귀여운 생명체는
-    ## 무엇일까요?
-    """
-)
+# <추천 버튼>
+#
 
-# 이미지 링크 삽입
-
-
-
-st.write(
-    """
-    ## 귀엽죠?
-    """
-)
-
-st.image(
-        "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_1280.jpg"
-)
-
-
-st.image(
-            "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg"
-)
-
-
-st.image(
-    "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg"
-)
-
-
-st.image(
-    "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
-)
-
-
-
-st.image(
-    "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_1280.jpg"
-)
-
-st.write(
-    """
-
-
-
-
-    ## 정답은 인간입니다.
-
-    """
-)
-
-st.image(
-    "https://cdn.pixabay.com/photo/2022/12/13/03/12/03-12-04-105_1280.jpg"
-)
+# st.write() 마크 다운
+st.title("조 추첨 페이지")
