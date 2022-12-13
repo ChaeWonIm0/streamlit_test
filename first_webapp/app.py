efb6fb6386b4fb2b406610ea0182e8ffe5406484
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 11:56:38 +0900

    streamlit

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -6,12 +6,13 @@ st.write(
     """ 
     # 제 첫 웹페이지입니다.
     ## 부족하지만 많이 사랑해주십쇼
-    기부는 1$씩
-    ### 1$ 는 1300원 입니다.
+    ###기부는 1$씩
+    #### 1$ 는 1300원 입니다.
     """
 )
 
 # 이미지 링크 삽입
 st.image(
+    "C:\Users\임채원\Documents\카카오톡 받은 파일\KakaoTalk_20221206_131801758.jpg"
     "https://pixabay.com/get/gcaa95692587d71a176aee58233f8ede0154d1351fa34e8d9bfa408fd0cd5124a209e4b9556a0f548b30efd115fad7ce6627891860343018a36f87c628a0f92d2_1920.jpg"
 )
