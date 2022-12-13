a45f92329a5646c7397772d2ab6aa3a5c4b0429a
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 17:03:26 +0900

    g

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -5,13 +5,14 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-titanic = sns.load_dataset('titanic')
-st.write(titanic) # 적당히 짤라줌
+import streamlit as st
+from streamlit_image_comparison import image_comparison
+
 
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """ 
-    ## 서울의 과거 사진
+    ## 서울의 과거 사진 비교
     ## 
     """
 )
@@ -21,7 +22,7 @@ st.write(
 
 st.write(
     """
-    ## 귀엽죠?
+    ## 한강
     """
 )
 
@@ -35,28 +36,10 @@ st.image(
 )
 
 
-st.image(
-    "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg"
-)
-
-
-st.image(
-    "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
-)
-
-
-
-st.image(
-    "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_1280.jpg"
-)
 
 st.write(
     """
-
-
-
-
-    ## 정답은 인간입니다.
+    ## 청계천
 
     """
 )
