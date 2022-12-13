9fd3d85d32b6284a233fcb164b2cefe41aa2fc2c
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 12:03:29 +0900

    streamlit

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -18,3 +18,7 @@ st.image(
 st.image(
             "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg"
 )
+
+st.image(
+    "file:///C:/Users/%EC%9E%84%EC%B1%84%EC%9B%90/Downloads/%EB%8C%91(@dap.chaewon)%20%E2%80%A2%20Instagram%20%EC%82%AC%EC%A7%84%20%EB%B0%8F%20%EB%8F%99%EC%98%81%EC%83%81.html"
+)
