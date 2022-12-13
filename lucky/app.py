569387abdc840492cbf4bfbc70d6f2f8659ac91c
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 12:34:26 +0900

    st.header

--- lucky/app.py
+++ lucky/app.py
@@ -2,11 +2,13 @@
 import streamlit as st
 import numpy as np
 
-# 추첨 대상인 13명의 이름을 넣을 수 있는 
+# https://docs.streamlit.io/library/get-started/main-concepts
+# st.write() 마크다운
+st.title("조추첨 페이지")
+st.header("여러분의 참여를 환영합니다!")
+
+# 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 13명이 소속될 조 이름을 넣을 위치
 
-# <추천 버튼>
-#
-
-# st.write() 마크 다운
-st.title("조 추첨 페이지")
+# <추첨 버튼>
+# 13개의 짝을 지어서 표시해줄 그래픽
