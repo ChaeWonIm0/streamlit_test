365d91923ee0dac1fe888d2ea492b254686140ea
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 14:37:47 +0900

    d

--- lucky/app.py
+++ lucky/app.py
@@ -51,23 +51,21 @@ for idx, col in enumerate(columns2): # 열의 위치
 # st.write(st.session_state)
 # np.random.choice -> 추출해서 이름들, 목록
 # 1. st.session_state - n, g가 섞여있음
-ss = pd.Series(st.session_state)
-#st.write(ss)
+ss = pd.Series(st.session_state) # 딕셔너리 -> 시리즈
+# st.write(ss)
+# ss2 = ss[ss != ""]
 ss2 = ss[ss.ne("")]
-#st.write(ss2)
-# string과 관련된 메서드를 사용할 수 있게 합니다
+# st.write(ss2)
+
+# str: string 관련된 메소드를 사용할 수 있게 함
 n_idx = ss2.index.str.contains('n')
-n_data = ss2[n_index]
-#st.write(n_data)
+n_data = ss2[n_idx]
+# st.write(n_data)
 
 g_idx = ss2.index.str.contains('g')
-g_data = ss2[g_index]
-#st.write(g_data)
+g_data = ss2[g_idx]
+# st.write(g_data)
 
-# n_data를 섞어줄 것입니다
-np.random.choice(n_data, len(n_data), replace = False)
+# n_data를 섞어줄 것임 (비복원으로)
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
 st.write(n_rd)
-
-# 2. df 형태로 정리
-# <추첨 버튼>
-# 13개의 짝을 지어서 표시해줄 그래픽
