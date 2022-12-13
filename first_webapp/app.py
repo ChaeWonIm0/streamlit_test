5b31c300c5ba8e94d8a962a32ced1ea90cdf2a83
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 16:37:03 +0900

    t

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -61,11 +61,4 @@ st.image(
 
 
 
-import streamlit as st
-from streamlit_image_comparison import image_comparison
-import cv2
 
-
-st.set_page_config("Webb Space Telescope vs Hubble Telescope", "🔭")
-streamlit 
-streamlit-image-comparison
