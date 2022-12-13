fd769ddf7dd99b45090c36d115495f2837d3ed69
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 23:17:02 +0900

    s

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -93,7 +93,7 @@ import plotly.express as px
 # st.pyplot(fig)
 
 st.header("Size of the fashion market for all items")
-st.bar_chart(fashion, width = 150, height = 600, rot = 90)
+st.bar_chart(fashion, width = 150, height = 600)
 
 # st.header("Male formal Market size")
 # st.bar_chart(fashion, x="Male_formal", y="2020", color="2021")
