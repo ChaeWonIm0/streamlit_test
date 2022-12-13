8e950946199dc16eac69ff317b6cd66179d4cd8c
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 14:25:25 +0900

    ㅇ

--- lucky/app.py
+++ lucky/app.py
@@ -11,10 +11,10 @@ st.header("여러분의 참여를 환영합니다!")
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
-tabs = st.tabs(['참가자','조'])
+tabs = st.tabs(['참가자', '조'])
 
-# 0번째 탭에 컬럼을 넣겠다
-columns = tabs[0].columns(4) # 화면을 열로 나누어서 배치합니다.
+# 0번째 탭에 컬럼(열)을 넣겠다
+columns = tabs[0].columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
 # enumerate : index, value 묶음
@@ -26,15 +26,15 @@ for idx, col in enumerate(columns): # 열의 위치
         # col 안에 메소드를 통해서 요소들을 생성해주겠다
         col.text_input(
             f"조 추첨 대상 {idx+1 + idx2 * 4}",
-            key=f"넘버{idx+1 + idx2 * 4}"
+            key=f"n{idx+1 + idx2 * 4}"
         ) # 4번 호출됨
-
 # 2번째 : 조
-# tabs[수], columns[수]바꿈, key=f"조" 바꿈
-columns = tabs[1].columns(4) # 화면을 열로 나누어서 배치합니다.
+# columns -> columns2, tabs[0] -> tabs[1]
+columns2 = tabs[1].columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
 # enumerate : index, value 묶음
+# columns -> columns2
 for idx, col in enumerate(columns2): # 열의 위치
     # 이중 for문 (for문 안에 for문)
     # col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
@@ -43,13 +43,13 @@ for idx, col in enumerate(columns2): # 열의 위치
         # col 안에 메소드를 통해서 요소들을 생성해주겠다
         col.text_input(
             f"조 목록 {idx+1 + idx2 * 4}",
-            key=f"조{idx+1 + idx2 * 4}"
+            key=f"g{idx+1 + idx2 * 4}" # n -> g => 겹치지 말라고
         ) # 4번 호출됨
 
-
+# 13명이 소속될 조 이름을 넣을 위치
+st.write(st.session_state)
 # <추첨 버튼>
 # 13개의 짝을 지어서 표시해줄 그래픽
-st.write(st.session_state)
 
 
 st.image(
