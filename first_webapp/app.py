5d86b95cc460f2e98025a02ee0f66bf8beaf9da5
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 19:11:02 +0900

    i

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -52,5 +52,5 @@ with col4:
     st.header("Winter")
     st.image("./2020 image/2020패션 겨울.jpg")
 
-<a href="https://homzzang.com">홈짱닷컴</a><br>
+<a href="https://homzzang.com"></a><br>
 <a href="https://homzzang.com"><img src="https://source.unsplash.com/random"></a>
