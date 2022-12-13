f718c91e54d2a5722c34e74b2b26ab28db081f1e
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 12:17:15 +0900

    streamlit

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -17,22 +17,56 @@ st.image(
     "https://cdn.pixabay.com/photo/2022/12/13/03/12/03-12-04-105_1280.jpg"
 )
 
+st.write(
+    """
+    ## 여러분의 기부는
+    """
+)
+
 st.image(
         "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_1280.jpg"
+)
+
+st.write(
+    """
+    ## 귀여운 고양이를
+    """
+
+
 )
 st.image(
             "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg"
 )
 
+st.write(
+    """
+    ## 살립니다
+    """
+)
 st.image(
     "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg"
+)
+
+st.write(
+    """
+    ## 많은 관심과 사랑에
+    """
 )
 
 st.image(
     "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
 )
 
+st.write(
+    """
+    ## 감사합니다.
+    """
+)
+
 st.image(
     "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_1280.jpg"
 )
 
+st.image(
+    "https://cdn.pixabay.com/photo/2017/11/10/15/04/steak-2936531_1280.jpg"
+)
