637170c7011088099ecff5353980762eb3cdadf7
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 12:23:59 +0900

    streamlit

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -4,22 +4,18 @@ import streamlit as st
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """ 
-    # 제 첫 웹페이지입니다.
-    ## 부족하지만 많이 사랑해주십쇼
-    ### 기부는 1$씩
-    #### 1$ 는 1300원 입니다.
+    # 세상에서 가장 귀여운 생명체는
+    ## 무엇일까요?
     """
 )
 
 # 이미지 링크 삽입
 
-st.image(
-    "https://cdn.pixabay.com/photo/2022/12/13/03/12/03-12-04-105_1280.jpg"
-)
+
 
 st.write(
     """
-    ## 여러분의 기부는
+    ## 귀엽죠?
     """
 )
 
@@ -27,41 +23,22 @@ st.image(
         "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_1280.jpg"
 )
 
-st.write(
-    """
-    ## 귀여운 고양이를
-    """
 
-
-)
 st.image(
             "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg"
 )
 
-st.write(
-    """
-    ## 살립니다
-    """
-)
+
 st.image(
     "https://cdn.pixabay.com/photo/2017/08/07/22/57/coffee-2608864_1280.jpg"
 )
 
-st.write(
-    """
-    ## 많은 관심과 사랑에
-    """
-)
 
 st.image(
     "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
 )
 
-st.write(
-    """
-    ## 감사합니다.
-    """
-)
+
 
 st.image(
     "https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_1280.jpg"
@@ -70,11 +47,16 @@ st.image(
 st.write(
     """
 
-    ## 기부 성공
+
+
+
+    ## 정답은 인간입니다.
 
     """
 )
 
 st.image(
-    "https://cdn.pixabay.com/photo/2017/11/10/15/04/steak-2936531_1280.jpg"
+    "st.image(
+    "https://cdn.pixabay.com/photo/2022/12/13/03/12/03-12-04-105_1280.jpg"
+)"
 )
