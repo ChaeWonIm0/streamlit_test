e64866ae4d832855a449af5a54f76b91d73f1382
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-13 14:20:47 +0900

    ㅇ

--- lucky/app.py
+++ lucky/app.py
@@ -12,7 +12,9 @@ st.header("여러분의 참여를 환영합니다!")
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
 tabs = st.tabs(['참가자'])
-columns = st.columns(4) # 화면을 열로 나누어서 배치합니다.
+
+# 0번째 탭에 컬럼을 넣겠다
+columns = tabs[0].columns(4) # 화면을 열로 나누어서 배치합니다.
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
 # enumerate : index, value 묶음
