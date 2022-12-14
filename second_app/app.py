28bfa4fb0cb013ecd8b995934e7654300883cf04
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 11:08:37 +0900

    e

--- second_app/app.py
+++ second_app/app.py
@@ -96,4 +96,4 @@ fashion_market = pd.pivot_table(fashion, index = 'index')
 st.write(fashion_market)
 
 st.header("Male formal Market size")
-st.bar_chart(fashion, x="2020")
+st.bar_chart(fashion_market, x="2020")
