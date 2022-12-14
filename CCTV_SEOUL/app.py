b6601ebf596daa41b2cac36d3d0d1fa9bb2305a1
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 12:25:43 +0900

    e

--- CCTV_SEOUL/app.py
+++ CCTV_SEOUL/app.py
@@ -7,8 +7,8 @@ st.header("서울시 CCTV 현황 분석")
 CCTV_Seoul = pd.read_csv('./CCTV_SEOUL/dataframe/CCTV_in_Seoul.csv', encoding='utf-8')
 st.write(CCTV_Seoul)
 
-CCTV_Seoul
+st.line_chart(CCTV_Seoul)
 
-import plotly.express as px
-fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-fig.show()
+# import plotly.express as px
+# fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+# fig.show()
