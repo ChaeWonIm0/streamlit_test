d226e1c8995e6b84b99d0ecd23291b412e8eb2f3
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 12:37:59 +0900

    e

--- CCTV_SEOUL/app.py
+++ CCTV_SEOUL/app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import numpy as numpy
+import pandas as pandas
+
 import plotly.graph_objects as go
 
 labels = ['Male_formal','Female_formal','Casual','Sprots','Inner','kids','Shoes']
