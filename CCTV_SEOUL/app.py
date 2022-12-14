5af51d6917ac5f581b399e27be67b41a82646785
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 12:14:39 +0900

    t

--- CCTV_SEOUL/app.py
+++ CCTV_SEOUL/app.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 st.header("서울시 CCTV 현황 분석")
 
-CCTV_Seoul = pd.read_csv('CCTV_SEOUL/dataframe/CCTV_in_Seoul.csv', encoding='utf-8')
+CCTV_Seoul = pd.read_csv('./CCTV_SEOUL/dataframe/CCTV_in_Seoul.csv', encoding='utf-8')
 st.write(CCTV_Seoul)
 
 CCTV_Seoul.columns
