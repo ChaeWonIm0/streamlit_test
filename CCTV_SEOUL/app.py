80682885eabfefb5b20d134486d3ab9cd9151a7c
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 12:35:22 +0900

    ㄱ

--- CCTV_SEOUL/app.py
+++ CCTV_SEOUL/app.py
@@ -1,15 +1,5 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.header("서울시 CCTV 현황 분석")
-
-CCTV_Seoul = pd.read_csv('./CCTV_SEOUL/dataframe/CCTV_in_Seoul.csv', encoding='utf-8')
-st.write(CCTV_Seoul)
-
-
-st.bar_chart(CCTV_Seoul)
-
-# import plotly.express as px
-# fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-# fig.show()
+import plotly.graph_objects as go
+labels = ['Male_formal','Female_formal','Casual','Sprots','Inner','kids','Shoes']
+values = [1921,1359,8311,2697,964,584,3386]
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+st.plotly_chart(fig)
