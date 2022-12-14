375a8628d174fdedcfd709496e94e93e35e4bc1c
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-14 12:13:12 +0900

    e

---
+++ CCTV_SEOUL/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.header("서울시 CCTV 현황 분석")
+
+CCTV_Seoul = pd.read_csv('CCTV_SEOUL/dataframe/CCTV_in_Seoul.csv', encoding='utf-8')
+st.write(CCTV_Seoul)
