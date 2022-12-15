f41369264d08645c63ee4bef1160676e1a87ddd7
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-15 17:08:23 +0900

    ml

--- ML_model/app.py
+++ ML_model/app.py
@@ -7,3 +7,8 @@ st.header ("First Machine Learning Linear regression")
 
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+import joblib
+model = joblib.load('./ML_model/app.py')
+model_info = pd.Series(model.coef_, index = df.drop['expenses'].X.columns)
+st.write(model_info)
