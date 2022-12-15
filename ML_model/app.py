423c8c236669584095e682c808f6250632665342
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-15 17:21:14 +0900

    ml

--- ML_model/app.py
+++ ML_model/app.py
@@ -9,7 +9,29 @@ df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/d
 st.write(df)
 
 import joblib
-model = joblib.load('./ML_model/app.py')
-model_info = pd.Series(model.coef_, index = df.drop['expenses'].columns)
-st.write(model_info)
+import os
 
+# os.path : 파이썬 경로 문제 해결
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+
+# age : 나이
+st.number_input(
+    label="나이",
+    step=1, 
+    value=30,
+    key='age'
+)
+# st.session_state['age']
+# st.write(st.session_state['age'])
+
+# sex : 성별
+st.radio(
+    label='성별'
+    options=["남성", "여성"],
+    index=0 # 기본 선택
+    key='sex'
+)
+st.write(st.session_state['sex'])
