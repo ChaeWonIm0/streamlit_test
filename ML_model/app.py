109650c139b9314b83a1b3e47a33fa9692642855
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-15 17:03:09 +0900

    ml_model

--- ML_model/app.py
+++ ML_model/app.py
@@ -4,3 +4,6 @@ import numpy as np
 
 
 st.header ("First Machine Learning Linear regression")
+
+df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
+st.write(df)
