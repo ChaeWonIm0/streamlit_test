cc7d20c80320998a4595528a6a90710bcb6c845a
Chae Won Im <114221089+dla9944@users.noreply.github.com>
2022-12-24 13:02:40 +0900

    t

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.set_page_config("SIMMONS TERACE로 여러분을 초대합니다.")
 
 
-st.image("./2020 image/시몬스.png") #카메라
+st.image("./2020 image/시몬스.jpg") #카메라
 
 
 st.header("SIMMONS TERACE로 여러분을 초대합니다")
@@ -17,13 +16,13 @@ st.write("")
 "#### 시몬스테라스 낮 사진 예시"
 st.write("")
 
-st.image("./2020 image/시몬스2.png") 
+st.image("./2020 image/시몬스2.jpg") 
 
 st.write("")
 "#### 시몬스테라스 밤 사진 예시"
 st.write("")
 
-st.image("./2020 image/시몬스3.png") #카메라
+st.image("./2020 image/시몬스3.jpg") #카메라
 
 st.write("")
 "#### 시간 : 오후 15시~ 종료는 미정"
